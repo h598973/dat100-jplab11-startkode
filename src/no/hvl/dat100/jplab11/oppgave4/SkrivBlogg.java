be57b239b09e2529bc5283984048a27b7f16a2c4
597663 <597663@stud.hvl.no>
2021-11-19 21:16:30 +0100

    Oppgave 4

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -2,14 +2,29 @@ package no.hvl.dat100.jplab11.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.File;
 
-import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		File fil = new File(mappe + filnavn);
+
+		try {
+
+			PrintWriter skriver = new PrintWriter(fil);
+
+			skriver.print(samling.toString());
+
+			skriver.close();
+
+			return true;
+
+		} catch (FileNotFoundException e) {
+			return false;
+
+		}
 	}
 }
